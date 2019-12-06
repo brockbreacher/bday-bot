ca3236f267477e650d2d7029783564cbedad2838
Yamboy1 <yamboyd1@gmail.com>
2019-12-07 05:08:10 +1300

    Add ping as prefix

--- src/cmd/CommandFactory.ts
+++ src/cmd/CommandFactory.ts
@@ -1,15 +1,16 @@
 import { Client, Message } from "discord.js";
 import { Config } from "../Config";
-import { Command} from "./Command";
+import { Command } from "./Command";
 import { PingCommand } from "./cmds/PingCommand";
 
 export class CommandFactory {
 	constructor(readonly client: Client) {}
 
 	create(message: Message): Command | null {
-		const prefix = Config.getValue("prefix");
-		if (!message.content.startsWith(prefix)) return null;
-		const [command, ...args] = message.content.slice(prefix.length).split(/ +/);
+		const content = message.content;
+		const prefix = [Config.getValue("prefix"), `${this.client.user}`].find(x => content.startsWith(x));
+		if (!prefix) return null;
+		const [command, ...args] = content.slice(prefix.length).trim().split(/ +/);
 
 		switch (command) {
 			case "ping":
