9f95dfc4898d1a2c19a6cddc48d6d39a640aa00c
Yamboy1 <yamboyd1@gmail.com>
2019-12-11 04:19:51 +1300

    Fix up warnings

--- src/database/entity/User.ts
+++ src/database/entity/User.ts
@@ -3,10 +3,8 @@ import { Column, Entity, PrimaryColumn } from "typeorm";
 @Entity()
 export class User {
 	@PrimaryColumn({ length: 18 })
-		// @ts-ignore
-	id: string;
+	id!: string;
 
 	@Column()
-		// @ts-ignore
-	birthday: Date;
+	birthday!: Date;
 }
