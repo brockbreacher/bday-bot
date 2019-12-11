680266423490afce46fcd1e070b492bea54cc994
Yamboy1 <yamboyd1@gmail.com>
2019-12-11 15:40:29 +1300

    Remove unncessary stuff

--- src/database/entity/User.ts
+++ src/database/entity/User.ts
@@ -1,13 +1,9 @@
-import { Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
-import { Guild } from ".";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class User {
 	@PrimaryColumn({ length: 18 })
 	id!: string;
-
-	@ManyToOne(() => Guild)
-	guild!: Guild;
 
 	@Column()
 	birthday!: Date;
