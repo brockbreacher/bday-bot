d25e08460d222db4eeeec28b54c1c890a38636c2
Yamboy1 <yamboyd1@gmail.com>
2019-12-14 14:47:34 +1300

    Formatting

--- src/handlers/TimerHandler.ts
+++ src/handlers/TimerHandler.ts
@@ -3,9 +3,10 @@ import { AnnouncementHandler } from ".";
 
 export class TimerHandler {
 	private readonly announcementHandler = new AnnouncementHandler(this.client);
+
 	constructor(readonly client: Client) {}
 
 	start(time: Date) {
-		setTimeout(() => this.announcementHandler.announce(),time.getTime() - Date.now())
+		setTimeout(() => this.announcementHandler.announce(), time.getTime() - Date.now())
 	}
 }
