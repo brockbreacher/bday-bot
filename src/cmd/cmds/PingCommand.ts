2a8c40b5b324e7f0398a88428596ceadf759ddf9
Yamboy1 <yamboyd1@gmail.com>
2019-12-20 11:35:42 +1300

    Switch to embeds

--- src/cmd/cmds/PingCommand.ts
+++ src/cmd/cmds/PingCommand.ts
@@ -1,9 +1,16 @@
+import { Message, RichEmbed } from "discord.js";
 import { Command } from "../Command";
 
 export class PingCommand extends Command {
 	static readonly identifier = "ping";
 
 	async run() {
-		await this.message.channel.send("Ping!");
+		const embed = new RichEmbed().setTitle("Ping").setDescription("Pinging server...").setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(14035250).setTimestamp();
+		const newMessage = await this.message.channel.send(embed) as Message;
+
+		const pingDelay = newMessage.createdTimestamp - this.message.createdTimestamp;
+		console.log(pingDelay);
+		const editedEmbed = new RichEmbed().setTitle("Pong!").setDescription(`Ping: \`${pingDelay}ms\``).setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(58390).setTimestamp();
+		await newMessage.edit(editedEmbed);
 	}
 }
