dd81d5f58d1e32b70c711371cb5fbacbe61a12b6
Yamboy1 <yamboyd1@gmail.com>
2019-12-20 15:55:45 +1300

    Switch setupguild command to embeds

--- src/cmd/cmds/SetupGuild.ts
+++ src/cmd/cmds/SetupGuild.ts
@@ -1,5 +1,5 @@
 import { Command } from "../Command";
-import { Message } from "discord.js";
+import { User as DiscordUser, Message, RichEmbed, TextChannel } from "discord.js";
 import { Guild } from "../../database/entity";
 import { getRepository } from "typeorm";
 
@@ -10,23 +10,43 @@ export class SetupGuild extends Command {
 		const repository = getRepository(Guild);
 
 		if (!this.message.member.hasPermission("MANAGE_GUILD")) {
-			return void await this.message.reply("you need to have the `MANAGE_GUILD` permission to run this command.");
+			const errorEmbed = new RichEmbed().setTitle("Invalid Permissions").setDescription("You need `Manage Server` permissions to run this command").setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(1403525).setTimestamp();
+			return this.message.channel.send(errorEmbed);
 		}
 
-		await this.message.channel.send("What channel would you like me to announce in?");
-		const filter = (m: Message) => m.author.id === this.message.author.id;
-		const responses = await this.message.channel.awaitMessages(filter, { max: 1, time: 30000 });
-		if (!responses.size) return void await this.message.reply("command cancelled.");
-		const input = responses.first().content;
+		const embed = new RichEmbed().setTitle("Announcement Channel").setDescription(`Please enter the channel that you would like me to announce birthdays in.\ne.g. ${this.message.channel} or the channel id: \`${this.message.channel.id}\``).setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(16753920).setTimestamp();
+		const input = await this.promptMessage(embed, this.message.channel as TextChannel, this.message.author);
+		if (!input) {
+			const errorEmbed = new RichEmbed().setTitle("Prompt timeout").setDescription("You waited too long. Command cancelled!").setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(14035250).setTimestamp();
+			return this.message.channel.send(errorEmbed);
+		}
 
-		const channelId = input.match(/(\d{18})/)?.[1];
-		if (!channelId || !this.message.guild.channels.has(channelId)) return void await this.message.reply("invalid channel.");
+		const channelId = this.parseInput(input);
+		if (!channelId) {
+			const embed = new RichEmbed().setTitle("Invalid Channel").setDescription("That is not a valid channel in this server.").setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(14035250).setTimestamp();
+			return this.message.channel.send(embed);
+		}
 
 		const guild = new Guild();
 		guild.id = this.message.guild.id;
 		guild.announcementChannel = channelId;
 		await repository.save(guild);
-		await this.message.channel.send("This guild has been set up successfully.");
+		const successEmbed = new RichEmbed().setTitle("Guild Set Up Sucessfully").setDescription("This guild has been set up successfully").setFooter("Service provided by Bday-Bot", this.client.user.displayAvatarURL).setColor(58390).setTimestamp();
+		await this.message.channel.send(successEmbed);
+	}
 
+	private async promptMessage(content: string | RichEmbed, channel: TextChannel, user: DiscordUser) {
+		await channel.send(content);
+		const filter = (m: Message) => m.author.id === user.id;
+		const responses = await channel.awaitMessages(filter, { max: 1, time: 30_000 });
+		if (!responses.size) return null;
+		return responses.first().content;
+	}
+
+	private parseInput(input: string) {
+		const channelId = input.match(/(\d{18})/)?.[1];
+
+		if (!channelId || !this.message.guild.channels.has(channelId)) return null;
+		return channelId;
 	}
 }
