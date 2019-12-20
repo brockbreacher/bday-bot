c681aeb80182f31e7ecfdbcdd8943a78db090cc8
Yamboy1 <yamboyd1@gmail.com>
2019-12-20 22:39:04 +1300

    Add locks and extra logging

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -1,23 +1,27 @@
-import { Client, Message } from "discord.js";
+import { Client, Collection, Message, User } from "discord.js";
 import { CommandFactory } from "../cmd/CommandFactory";
 
 export class CommandHandler {
 	factory = new CommandFactory(this.client);
+	userLock = new Collection<string, User>();
 
 	constructor(readonly client: Client) {}
 
 	async process(message: Message): Promise<void> {
 		if (message.author.bot) return;
+		if (this.userLock.has(message.author.id)) return;
 
 		const command = this.factory.create(message);
 		if (command === null) return;
 
-		console.log(`Running ${command.identifier}!`);
+		this.userLock.set(message.author.id, message.author);
+		console.log(`[${message.author.id}] Running ${command.identifier}!`);
 		try {
 			await command.run();
-			console.log(`Ran ${command.identifier}!`);
+			console.log(`[${message.author.id}] Ran ${command.identifier}!`);
 		} catch (e) {
-			console.log(`Error running ${command.identifier}: ${e}`);
+			console.log(`[${message.author.id}] Error running ${command.identifier}: ${e.stack}`);
 		}
+		this.userLock.delete(message.author.id);
 	}
 }
