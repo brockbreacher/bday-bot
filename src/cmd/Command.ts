d924fbb8d4451b862d0ab456665c7a0cb485ca51
Yamboy1 <yamboyd1@gmail.com>
2019-12-28 21:45:08 +1300

    Small refactor

--- src/cmd/Command.ts
+++ src/cmd/Command.ts
@@ -3,11 +3,11 @@ import { GuildMessage } from "../util/GuildMessage";
 import { ResponseFactory } from "../response/ResponseFactory";
 
 export abstract class Command {
+	get identifier() { return (this.constructor as typeof Command).identifier };
+	get description() { return (this.constructor as typeof Command).description };
+
 	static readonly identifier: string = "invalid-command";
 	static readonly description: string = "Invalid Command";
-
-	get identifier() { return (this.constructor as typeof Command).identifier };
-	get description() { return (this.constructor as typeof Command).description }
 
 	constructor(readonly client: Client, readonly args: string[], readonly message: GuildMessage) {}
 
